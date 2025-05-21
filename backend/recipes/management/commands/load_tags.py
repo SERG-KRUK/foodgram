63382248fe32aaaaba39f65a8de8ac4b9b17f6a7
Сергей Крюк <bmxelement777@yandex.ru>
2025-05-21 21:51:19 +0300

    flake8-5

--- backend/recipes/management/commands/load_tags.py
+++ backend/recipes/management/commands/load_tags.py
@@ -1,12 +1,14 @@
-from django.core.management.base import BaseCommand
+"""Модуль для загрузки тегов по умолчанию."""
 
+from django.core.management.base import BaseCommand
 from recipes.models import Tag
 
-
 class Command(BaseCommand):
+    """Команда для загрузки стандартных тегов в базу данных."""
     help = 'Load default tags'
 
     def handle(self, *args, **options):
+        """Загружает предустановленные теги в базу данных."""
         tags = [
             {'name': 'Завтрак', 'slug': 'breakfast'},
             {'name': 'Обед', 'slug': 'lunch'},
