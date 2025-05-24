31fdc0ec9eb34f969642eb0deda2bbf454a4eb32
Сергей Крюк <bmxelement777@yandex.ru>
2025-05-24 16:00:06 +0300

    admin panel fix

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,7 +1,9 @@
 from rest_framework.views import exception_handler
-from rest_framework.response import Response
+
 
 def custom_exception_handler(exc, context):
+    """api утилита для ошибки ингридиентов кол-ва"""
+
     response = exception_handler(exc, context)
 
     if response is not None:
