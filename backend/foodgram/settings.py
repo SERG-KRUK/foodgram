976837f4e3a390554b1e97731f537a8d88561259
Сергей Крюк <bmxelement777@yandex.ru>
2025-05-28 19:05:07 +0300

    bug

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
 
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'True'
+DEBUG = True
 
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
