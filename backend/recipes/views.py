e0790f7454459b60decd2c9442c21733facce1ba
Сергей Крюк <bmxelement777@yandex.ru>
2025-05-28 21:51:49 +0300

    reverse

---
+++ backend/recipes/views.py
@@ -0,0 +1,11 @@
+"""Вью рецептов для редиректа по короткой ссылке"""
+
+from django.shortcuts import get_object_or_404, redirect
+
+from .models import Recipe
+
+
+def recipe_by_short_link(request, short_link):
+    """Перенаправление по короткой ссылке на полный URL рецепта."""
+    recipe = get_object_or_404(Recipe, short_link=short_link)
+    return redirect(f'/recipes/{recipe.pk}/')
